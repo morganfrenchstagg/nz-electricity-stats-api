fa45f7b554042e34c5bdf69c45ebb13742fc5200
Morgan <morgan.french-stagg@authsignal.com>
2026-01-26 22:16:10 +1300

    ping me on slack if a new unit is added to RTD

--- src/Dispatch/syncDispatch.ts
+++ src/Dispatch/syncDispatch.ts
@@ -1,4 +1,5 @@
 import { env } from "cloudflare:workers";
+import { getGenerators } from "./generators";
 
 const emiApiUrl = 'https://emi.azure-api.net/real-time-dispatch/';
 
@@ -15,8 +16,12 @@ export async function syncDispatch() {
   }
 }
 
+function getNZDateTime(): Date{
+  return new Date(new Date().toLocaleString("en-US", { timeZone: "Pacific/Auckland" }));
+}
+
 function isSyncNeeded(lastSynced: Date){
-  const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Pacific/Auckland" }));
+  const now = getNZDateTime();
   const diff = now.getTime() - lastSynced.getTime();
   return diff / 1000 / 60 > 5;
 }
@@ -47,6 +52,7 @@ async function fetchFromEmiRtdApi(lastSynced?: Date){
     }
 
     await processEmiRtdData(data);
+    await checkForMissingUnits(data[0].FiveMinuteIntervalDatetime);
   } else {
     throw new Error(`Failed to fetch from EMI RTD API: ${response.statusText}`);
   }
@@ -57,4 +63,33 @@ async function processEmiRtdData(data: any){
     await env.DB.prepare(`INSERT INTO real_time_dispatch (PointOfConnectionCode, FiveMinuteIntervalDatetime, SPDLoadMegawatt, SPDGenerationMegawatt, DollarsPerMegawattHour) VALUES (?, ?, ?, ?, ?)`).bind(item.PointOfConnectionCode, item.FiveMinuteIntervalDatetime, item.SPDLoadMegawatt, item.SPDGenerationMegawatt, item.DollarsPerMegawattHour).run();
   }
   console.log("Processed " + data.length + " items");
+}
+
+async function checkForMissingUnits(lastSynced: string){
+  const generators = await getGenerators();
+  const dispatchList = await env.DB.prepare(`SELECT DISTINCT PointOfConnectionCode FROM real_time_dispatch WHERE FiveMinuteIntervalDatetime = ?`).bind(lastSynced).all();
+  const dispatchListResult = dispatchList.results.map(dispatch => dispatch.PointOfConnectionCode).filter(unit => (unit as string).split(' ').length > 1);
+
+  const unitsUnaccountedForInDispatchList = dispatchListResult;
+
+  for(const generator of generators){
+    for(const unit of generator.units){
+      if(dispatchListResult.includes(unit.node)){
+        unitsUnaccountedForInDispatchList.splice(unitsUnaccountedForInDispatchList.indexOf(unit.node), 1);
+      }
+    }
+  }
+
+  if(unitsUnaccountedForInDispatchList.length > 0){
+    console.log("Missing units: " + unitsUnaccountedForInDispatchList.join(', '));
+    await fetch(env.SLACK_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: "Missing units: `" + unitsUnaccountedForInDispatchList.join('`, `') + "`"
+      })
+    });
+  }
 }
