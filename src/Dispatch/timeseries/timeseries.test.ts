c97eb91b9bb2d6bb75c25e296ceef9e0f434b345
Morgan <morgan.french-stagg@authsignal.com>
2026-05-29 13:57:51 +1200

    handle new nodes properly

--- src/Dispatch/timeseries/timeseries.test.ts
+++ src/Dispatch/timeseries/timeseries.test.ts
@@ -3,73 +3,118 @@ import { it, describe, expect } from "vitest";
 import { generateTimeseries } from "./timeseries";
 
 describe("generateTimeseries", () => {
-	it("empty dataset should generate a timeseries based on the input", () => {
-		const exampleRtdData = [
-			{
-			  "PointOfConnectionCode": "ABY0111",
-			  "FiveMinuteIntervalDatetime": "2026-05-29T13:30:00",
-			  "FiveMinuteIntervalNumber": 1,
-			  "RunDateTime": "2026-05-29T01:29:01",
-			  "SPDLoadMegawatt": 2.576,
-			  "SPDGenerationMegawatt": 0,
-			  "DollarsPerMegawattHour": 94.08
-			},
-			{
-			  "PointOfConnectionCode": "ARA2201 ARA0",
-			  "FiveMinuteIntervalDatetime": "2026-05-29T13:30:00",
-			  "FiveMinuteIntervalNumber": 1,
-			  "RunDateTime": "2026-05-29T01:29:01",
-			  "SPDLoadMegawatt": 0,
-			  "SPDGenerationMegawatt": 72,
-			  "DollarsPerMegawattHour": 100.27
-			}
-		  ];
+  it("empty dataset should generate a timeseries based on the input", () => {
+    const exampleRtdData = [
+      {
+        PointOfConnectionCode: "ABY0111",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:30:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 2.576,
+        SPDGenerationMegawatt: 0,
+        DollarsPerMegawattHour: 94.08,
+      },
+      {
+        PointOfConnectionCode: "ARA2201 ARA0",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:30:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 0,
+        SPDGenerationMegawatt: 72,
+        DollarsPerMegawattHour: 100.27,
+      },
+    ];
 
-		const timeseries = generateTimeseries({ series: [], data: [] }, exampleRtdData);
-		expect(timeseries).toEqual({
-			series: ["ABY0111", "ARA2201 ARA0"],
-			data: [
-				["2026-05-29T13:30:00", -2.576, 72],
-			]
-		});
-	});
+    const timeseries = generateTimeseries(
+      { series: [], data: [] },
+      exampleRtdData,
+    );
+    expect(timeseries).toEqual({
+      series: ["ABY0111", "ARA2201 ARA0"],
+      data: [["2026-05-29T13:30:00", -2.576, 72]],
+    });
+  });
 
-	it("existing dataset should generate a timeseries based on the input", () => {
-		const existingTimeseries = {
-			series: ["ABY0111", "ARA2201 ARA0"],
-			data: [
-				["2026-05-29T13:30:00", -2.576, 72],
-			]
-		};
+  it("existing dataset should generate a timeseries based on the input", () => {
+    const existingTimeseries = {
+      series: ["ABY0111", "ARA2201 ARA0"],
+      data: [["2026-05-29T13:30:00", -2.576, 72]],
+    };
 
-		const exampleRtdData = [
-			{
-			  "PointOfConnectionCode": "ABY0111",
-			  "FiveMinuteIntervalDatetime": "2026-05-29T13:35:00",
-			  "FiveMinuteIntervalNumber": 1,
-			  "RunDateTime": "2026-05-29T01:29:01",
-			  "SPDLoadMegawatt": 3,
-			  "SPDGenerationMegawatt": 0,
-			  "DollarsPerMegawattHour": 94.08
-			},
-			{
-			  "PointOfConnectionCode": "ARA2201 ARA0",
-			  "FiveMinuteIntervalDatetime": "2026-05-29T13:35:00",
-			  "FiveMinuteIntervalNumber": 1,
-			  "RunDateTime": "2026-05-29T01:29:01",
-			  "SPDLoadMegawatt": 0,
-			  "SPDGenerationMegawatt": 70,
-			  "DollarsPerMegawattHour": 100.27
-			}
-		  ];
+    const exampleRtdData = [
+      {
+        PointOfConnectionCode: "ABY0111",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:35:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 3,
+        SPDGenerationMegawatt: 0,
+        DollarsPerMegawattHour: 94.08,
+      },
+      {
+        PointOfConnectionCode: "ARA2201 ARA0",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:35:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 0,
+        SPDGenerationMegawatt: 70,
+        DollarsPerMegawattHour: 100.27,
+      },
+    ];
 
-		const timeseries = generateTimeseries(existingTimeseries, exampleRtdData);
-		expect(timeseries).toEqual({
-			series: ["ABY0111", "ARA2201 ARA0"],
-			data: [
-				["2026-05-29T13:30:00", -2.576, 72],
-				["2026-05-29T13:35:00", -3, 70],
-			]
-		});
-	});
+    const timeseries = generateTimeseries(existingTimeseries, exampleRtdData);
+    expect(timeseries).toEqual({
+      series: ["ABY0111", "ARA2201 ARA0"],
+      data: [
+        ["2026-05-29T13:30:00", -2.576, 72],
+        ["2026-05-29T13:35:00", -3, 70],
+      ],
+    });
+  });
+
+  it("existing dataset should generate a timeseries based on the input + new node", () => {
+    const existingTimeseries = {
+      series: ["ABY0111", "ARA2201 ARA0"],
+      data: [["2026-05-29T13:30:00", -2.576, 72]],
+    };
+
+    const exampleRtdData = [
+      {
+        PointOfConnectionCode: "ABY0111",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:35:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 3,
+        SPDGenerationMegawatt: 0,
+        DollarsPerMegawattHour: 94.08,
+      },
+      {
+        PointOfConnectionCode: "ARA2201 ARA0",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:35:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 0,
+        SPDGenerationMegawatt: 70,
+        DollarsPerMegawattHour: 100.27,
+      },
+      {
+        PointOfConnectionCode: "BRB0331 RUK99",
+        FiveMinuteIntervalDatetime: "2026-05-29T13:35:00",
+        FiveMinuteIntervalNumber: 1,
+        RunDateTime: "2026-05-29T01:29:01",
+        SPDLoadMegawatt: 8.82,
+        SPDGenerationMegawatt: 0,
+        DollarsPerMegawattHour: 111.5,
+      },
+    ];
+
+    const timeseries = generateTimeseries(existingTimeseries, exampleRtdData);
+    expect(timeseries).toEqual({
+      series: ["ABY0111", "ARA2201 ARA0", "BRB0331 RUK99"],
+      data: [
+        ["2026-05-29T13:30:00", -2.576, 72, null],
+        ["2026-05-29T13:35:00", -3, 70, -8.82],
+      ],
+    });
+  });
 });
--- src/Dispatch/timeseries/timeseries.ts
+++ src/Dispatch/timeseries/timeseries.ts
@@ -5,8 +5,25 @@ export function generateTimeseries(existingTimeseries: Timeseries, rtdData: Real
 	const series = rtdData.map((item: RealTimeDispatch) => item.PointOfConnectionCode);
 	const thisDispatchData = rtdData.map((item: RealTimeDispatch) => item.SPDGenerationMegawatt - item.SPDLoadMegawatt);
 
+	// there may be new generators in the rtd data, so this aligns the data based on the new series
+	// inserting nulls where relevant
+	const data = existingTimeseries.data.map((item: any[]) => {
+		const row = [item[0]];
+		series.forEach((seriesItem) => {
+			const oldSeriesIndex = existingTimeseries.series.indexOf(seriesItem);
+			if (oldSeriesIndex === -1) {
+				row.push(null);
+			} else {
+				row.push(item[oldSeriesIndex + 1]);
+			}
+		});
+		return row;
+	});
+
+	data.push([rtdData[0].FiveMinuteIntervalDatetime, ...thisDispatchData]);
+
 	return {
 		series,
-		data: [...existingTimeseries.data, [rtdData[0].FiveMinuteIntervalDatetime, ...thisDispatchData]],
+		data,
 	}
 }
